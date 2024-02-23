2f91eaf643884f091a6b179f4f4484554fff34d9
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2024-02-23 19:07:31 -0300

    refactor: :recycle: Componentizatin Header from home

---
+++ gear-meeting/src/components/Header.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import logotype from '../../assets/logotipo-row.png'
+import user from '../../assets/person.png'
+import { useWindowDimensions } from 'react-native';
+import { useAuth } from '../hooks/AuthProvider';
+import styled from 'styled-components/native';
+import API from '../services/APIService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Header = styled.View`
+  width: 100%;
+  height: 51px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 2;
+`
+
+const LogotypeImage = styled.Image`
+`
+
+const UserView = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: gray;
+  border: 2px;
+  border-color: purple;
+  margin-right: 5px;
+`
+
+const UserImage = styled.Image`
+  width: 35px;
+  height: 35px;
+  border-radius: 17.5px;
+`
+
+const Modal = styled.View`
+  background-color: #040D29;
+  right: 5px;
+  margin-top: 52px;
+  width: 200px;
+  height: 300px;
+  border-radius: 10px;
+  padding: 10px;
+`
+
+Modal.Background = styled.View`
+  background-color: #0000004b;
+  position: absolute;
+  top: 3px;
+  left: 0;
+  width: 100%;
+  height: ${({ windowHeight }) => windowHeight}px;
+  display: flex;
+  align-items: flex-end;
+  z-index: 4;
+`
+
+Modal.Button = styled.TouchableOpacity`
+  width: 100%;
+  height: 30px;
+  background-color: #d9d9d930;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`
+
+Modal.Text = styled.Text`
+  color: white;
+  font-size: 14px;
+`
+
+export default function CustomHeader() {
+  const [userModal, setUserModal] = useState(false);
+  const [image, setImage] = useState(null)
+  const { height: windowHeight } = useWindowDimensions();
+  const { signOut } = useAuth();
+
+  // useEffect(async () => {
+  //   fetchImage()
+  // }, [image]);
+
+  const fetchImage = async () => {
+    const userId = await AsyncStorage.getItem('userId');
+    const token = await AsyncStorage.getItem('token');
+
+    API.get(`/logins/${userId}/photo`, (err, data) => {
+    if(err) {
+      console.error(err);
+    } else {
+      const response = data.data
+      setImage(response);
+    }
+    }, token)
+  }
+
+  const handleOpenUserModal = () => {
+    setUserModal(true);
+  }
+
+  const handleCloseUserModal = () => {
+    setUserModal(false);
+  }
+
+  const handleLogout = async () => {
+    signOut()
+    const token = await AsyncStorage.getItem('token')
+
+    if(!token) {
+      navigation.na
+    }
+  }
+
+  return (
+    <>
+      {userModal === true ? (
+          <Modal.Background windowHeight={windowHeight}>
+            <LinearGradient
+              colors={['#000', '#040D29']}
+              style={
+                {
+                  width: 200,
+                  right: 5,
+                  marginTop: 52,
+                  borderRadius: 10,
+                  padding: 10,
+                  zIndex: 4
+                }
+              }
+            >
+              <Modal.Button
+                onPress={handleLogout}
+              >
+                <Modal.Text>Teste</Modal.Text>
+              </Modal.Button>
+
+              <Modal.Button
+                onPress={handleLogout}
+              >
+                <Modal.Text>Logout</Modal.Text>
+              </Modal.Button>
+
+              <Modal.Button
+                onPress={handleCloseUserModal}
+              >
+                <Modal.Text>Fechar</Modal.Text>
+              </Modal.Button>
+            </LinearGradient>
+          </Modal.Background >
+        ) : null
+        }
+        <Header>
+          <LogotypeImage source={logotype} alt='Logotype Gear Meeting' />
+          <UserView
+            onPress={handleOpenUserModal}
+          >
+            {image ? (
+              <UserImage source={{ uri: URL.createObjectURL(image) }} alt='User image' />
+            ) : (
+              <UserImage source={user} alt='User image' />
+            )}
+          </UserView>
+        </Header>
+    </>
+  )
+}
--- gear-meeting/src/screens/Home.jsx
+++ gear-meeting/src/screens/Home.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import headerHome from '../../assets/headerHome.png'
 import styled from 'styled-components/native';
-import logotype from '../../assets/logotipo-row.png'
-import user from '../../assets/user.heic'
-import { useWindowDimensions } from 'react-native';
-import { useAuth } from '../hooks/AuthProvider';
+import { ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import ticket from '../../assets/ticket.png'
+import maps from '../../assets/googlemaps.png'
+import cars from '../../assets/carrosEPilotos.png'
+import CustomCard from '../components/Card';
+import CustomHeader from '../components/Header';
 
 
 const SafeAreaView = styled.View`
@@ -16,170 +18,79 @@ const SafeAreaView = styled.View`
 const HeaderImage = styled.Image`
   margin-top: 50px;
   position: absolute;
-  top: 0;
+  top: -51px;
   width: 100%;
   z-index: 1;
 `
 
-const Header = styled.View`
+const Scroll = styled.View`
   width: 100%;
-  height: 51px;
   display: flex;
-  background-color: transparent;
-  flex-direction: row;
-  justify-content: space-between;
   align-items: center;
-  z-index: 2;
-`
-
-const LogotypeImage = styled.Image`
-`
-
-const UserView = styled.TouchableOpacity`
-  width: 40px;
-  height: 40px;
-  border-radius: 20px;
-  background-color: gray;
-  border: 2px;
-  border-color: purple;
-  margin-right: 5px;
-`
-
-const UserImage = styled.Image`
-  width: 35px;
-  height: 35px;
-  border-radius: 17.5px;
-`
-
-const Modal = styled.View`
-  background-color: #040D29;
-  right: 5px;
-  margin-top: 52px;
-  width: 200px;
-  height: 300px;
-  border-radius: 10px;
-  padding: 10px;
-`
-
-Modal.Background = styled.View`
-  background-color: #0000004b;
-  position: absolute;
-  top: 3px;
-  left: 0;
-  width: 100%;
-  height: ${({ windowHeight }) => windowHeight}px;
-  display: flex;
-  align-items: flex-end;
+  justify-content: center;
   z-index: 4;
 `
 
-Modal.Button = styled.TouchableOpacity`
+const CardViewPaddingTop = styled.View`
   width: 100%;
-  height: 30px;
-  background-color: #d9d9d930;
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 10px;
+  margin-top: 220px;
 `
 
-Modal.Text = styled.Text`
-  color: white;
-  font-size: 14px;
-`
-
-const ContainerHome = styled.View`
-  margin-top: 210px;
-  background-color: #ff0000;
-  width: 100%;
-  flex: 1;
-`
-
-export default function Home() {
-  const [userModal, setUserModal] = useState(false);
-  const { height: windowHeight } = useWindowDimensions();
-  const { signOut } = useAuth();
-
-  const handleOpenUserModal = () => {
-    setUserModal(true);
+export default function Home({ navigation }) {
+  const handleGoToTicketScreen = () => {
+    navigation.navigate('Ticket')
   }
 
-  const handleCloseUserModal = () => {
-    setUserModal(false);
-  }
-
-  const handleLogout = () => {
-    signOut()
-
+  const handleGoToMapsScreen = () => {
+    navigation.navigate('Map')
   }
 
   return (
     <>
       <SafeAreaView />
-      <HeaderImage source={headerHome} alt='Cars at a parking' />
-      {userModal === true ? (
-        <Modal.Background windowHeight={windowHeight}>
-          <LinearGradient
-            colors={['#000', '#040D29']}
-            style={
-              {
-                width: 200,
-                right: 5,
-                marginTop: 52,
-                height: 300,
-                borderRadius: 10,
-                padding: 10,
-                zIndex: 4
-              }
-            }
-          >
-            <Modal.Button
-              onPress={handleLogout}
-            >
-              <Modal.Text>Teste</Modal.Text>
-            </Modal.Button>
-
-            <Modal.Button
-              onPress={handleLogout}
-            >
-              <Modal.Text>Logout</Modal.Text>
-            </Modal.Button>
-
-            <Modal.Button
-              onPress={handleCloseUserModal}
-            >
-              <Modal.Text>Fechar</Modal.Text>
-            </Modal.Button>
-          </LinearGradient>
-        </Modal.Background >
-      ) : null
-      }
-      <Header>
-        <LogotypeImage source={logotype} alt='Logotype Gear Meeting' />
-        <UserView
-          onPress={handleOpenUserModal}
-        >
-          <UserImage source={user} alt='User image' />
-        </UserView>
-      </Header>
-
       <LinearGradient
-        colors={['#1400FF', '#17949C']}
+        colors={['#000','#040D29', '#1400FF', '#17949C']}
         style={
           {
             width: '100%',
-            marginTop: 210,
-            flex: 1,
-            position: 'relative',
             top: 0,
+            height: 1468,
             left: 0,
           }
         }
       >
+        <HeaderImage source={headerHome} alt='Cars at a parking' />
+        <CustomHeader />        
 
+        <ScrollView style={{ zIndex: 3 }}>
+          <Scroll>
+            <CardViewPaddingTop>
+              <CustomCard 
+                label={'Ingressos'}
+                onPress={handleGoToTicketScreen}
+                description={'Compre o seu ingresso com facilidade e praticidade'}
+                icon={ticket}
+              />
+            </CardViewPaddingTop>
+            <CustomCard 
+              label={'Mapa do Evento'}
+              onPress={handleGoToMapsScreen}
+              description={'Compre o seu ingresso com facilidade e praticidade'}
+              icon={maps}
+            />
+            <CustomCard 
+              label={'Carros e Pilotos'}
+              description={'Compre o seu ingresso com facilidade e praticidade'}
+              icon={cars}
+            />
+            <CustomCard 
+              label={'Ingressos'}
+              description={'Compre o seu ingresso com facilidade e praticidade'}
+              icon={ticket}
+            />
+          </Scroll>
+        </ScrollView>
       </LinearGradient>
-
     </>
   )
 }
